bce3d5a60c2ba5e0d8993c021d6eea9504e22acc
imjncarlson <imjncarlson@gmail.com>
2023-06-30 17:20:09 -0500

    Version 1.1.0

--- README.md
+++ README.md
@@ -3,19 +3,23 @@
  |   \ _  _ _ _  __ _ _ __ (_)__  \ \    / / |_  ___ _ _ ___ 
  | |) | || | ' \/ _` | '  \| / _|  \ \/\/ /| ' \/ -_) '_/ -_)
  |___/ \_, |_||_\__,_|_|_|_|_\__|   \_/\_/ |_||_\___|_| \___|
-       |__/                                                  
-                 
+       |__/                                                            
 ```
 
 Dynamically handle multiple WHERE clauses in your SQL statements
 
 ## Installation
 
-Coming soon
+```
+npm i sql-dynamic-where
+```
 
 ## Usage
 
-Coming soon
+1. Craft the SQL query string up until the point of the WHERE clauses
+2. Add all WHERE clause variables into the Dynamic Where package
+3. Append the Dynamic Where string to the end of your SQL query string
+4. Pass the Dynamic where values into whatever process you use to execute the query
 
 ## License
 
--- SQLDynamicWhere.js
+++ SQLDynamicWhere.js
@@ -1,11 +1,21 @@
 class SQLDynamicWhere{
 
-    static LogicalOperator = {
+    /**
+     * 
+     * Enumerated type for logical operators
+     * 
+     */
+    static Logic = {
         And: 'AND',
         Or: 'OR'
     };
     
-    static ComparisonOperator = {
+    /**
+     * 
+     * Enumerated type for comparison operators
+     * 
+     */
+    static Comparison = {
         Equals: '=',
         DoesNotEqual: '!=',
         GreaterThan: '>',
@@ -24,50 +34,38 @@ class SQLDynamicWhere{
     }
 
     /**
-     * 
-     * The new where clause parameter
-     * @param {LogicalOperator} logicalOperator
-     * @param {string} field
-     * @param {ComparisonOperator} comparisonOperator
-     * @param {*} value
-     * @param {Array} override
+     *
+     * Adds a new dynamic where clause 
+     *
+     * @param {Logic} logicalOperator - The logical operator of the where clause
+     * @param {string} field - The table field
+     * @param {Comparison} comparisonOperator - The comparison operator of the where clause
+     * @param {*} value - The value to compare against
+     * @param {Array} override - (Optional) An array of additional value overrides to ignore
      * 
      */
-    add(logicalOperator, field, comparisonOperator, value, override = null){
+    add(logicalOperator, field, comparisonOperator, value, override = []){
 
-        if (value !== null && typeof value !== 'undefined'){ 
+        // If there is no value or it is overrided then do not add it to the array
+        if (value === null || typeof value === 'undefined' || override.includes(value)) return;
 
-            this.whereClauses.push(
-                {
-                    logicalOperator: logicalOperator,
-                    field: field,
-                    comparisonOperator: comparisonOperator,
-                    value: value,
-                    override: override
-                }
-            );
-
-        }
-
-    }
-
-    getWhereClauses(){
-
-        // TODO FIX ISSUE WHERE I NEED TO ADD THE ACTUAL "WHERE" KEYWORD
-
-        var whereClausesString = "";
-
-        this.whereClauses.forEach(element => {
-            whereClausesString += ` ${element.logicalOperator} ${element.field} ${element.comparisonOperator} (?)`;
-        });
-
-        return whereClausesString;
+        // Otherwise store it in the override array
+        this.whereClauses.push(
+            {
+                logicalOperator: logicalOperator,
+                field: field,
+                comparisonOperator: comparisonOperator,
+                value: value,
+                override: override
+            }
+        );
 
     }
 
     /**
      * 
-     * Returns the where clauses
+     * Returns an array of all the where clauses values. 
+     * Values will be in the order they were added in.
      * 
      */
     getValues(){
@@ -79,6 +77,77 @@ class SQLDynamicWhere{
         });
 
         return onlyValues;
+
+    }
+
+    /**
+     * 
+     * @param {string} baseQuery - The current portion of the SQL query. Usually containing the SELECT and FROM clauses
+     * @returns A SQL query safe string that contains all the where clauses
+     * 
+     */
+    getClauses(baseQuery){
+
+        // TODO Add override for insert symbol?
+        // TODO Add option to insert values over symbol?
+
+        var clauses = "";
+
+        // If no "WHERE" keyword present in the base query
+        if (!baseQuery.toLowerCase().includes("where")){
+            
+            // Add WHERE keyword
+            baseQuery += ' WHERE ';
+
+            // Add clauses
+            for (let i = 0; i < this.whereClauses.length; i++){
+
+                // Skip logical operator on the first clause
+                if (i === 0 ) clauses += ` ${this.whereClauses[i].field} ${this.whereClauses[i].comparisonOperator} (?)`;
+                else clauses += ` ${this.whereClauses[i].logicalOperator} ${this.whereClauses[i].field} ${this.whereClauses[i].comparisonOperator} (?)`;
+                
+            }
+
+        }
+        // If "WHERE" keyword already present in the base query
+        else {
+
+            // Split base query into parts using WHERE keyword as the delimiter
+            splitBaseQuery = baseQuery.toLowerCase().split('where')
+
+            // If there are multiple WHERE keywords in the base query throw an error
+            if (splitBaseQuery.length !== 2) throw new Error("Error crafting where clauses! Base query contains too many \'where\' keywords");
+
+            // If there are no clauses present
+            if (splitBaseQuery[1].trim().length === 0){
+                
+                // Add clauses
+                for (let i = 0; i < this.whereClauses.length; i++){
+
+                    // Skip logical operator on the first clause
+                    if (i === 0 ) clauses += ` ${this.whereClauses[i].field} ${this.whereClauses[i].comparisonOperator} (?)`;
+                    else clauses += ` ${this.whereClauses[i].logicalOperator} ${this.whereClauses[i].field} ${this.whereClauses[i].comparisonOperator} (?)`;
+                    
+                }
+
+            }
+            // If there are some clauses already present
+            else {
+
+                // Add clauses
+                for (let i = 0; i < this.whereClauses.length; i++){
+
+                    // Skip logical operator on the first clause
+                    clauses += ` ${this.whereClauses[i].logicalOperator} ${this.whereClauses[i].field} ${this.whereClauses[i].comparisonOperator} (?)`;
+                    
+                }
+                
+            }
+
+        }
+
+        return clauses;
+
     }
 
 }
